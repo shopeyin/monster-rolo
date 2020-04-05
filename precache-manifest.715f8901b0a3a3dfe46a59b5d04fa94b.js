e7a456b6163907723af6aa94d17f880bdb86bb1e
oladimeji <tommyslipz@gmail.com>
2020-04-05 06:08:02 +0100

    Updates

---
+++ precache-manifest.715f8901b0a3a3dfe46a59b5d04fa94b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3a5a555c1222b7103983594ed61882",
+    "url": "/monster-rolo/index.html"
+  },
+  {
+    "revision": "4d287f537868106d4495",
+    "url": "/monster-rolo/static/css/main.b6b2dcf0.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monster-rolo/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolo/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d287f537868106d4495",
+    "url": "/monster-rolo/static/js/main.216f1e24.chunk.js"
+  },
+  {
+    "revision": "5aef1382df9ad7d68004",
+    "url": "/monster-rolo/static/js/runtime-main.9a51a1a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolo/static/media/logo.5d5d9eef.svg"
+  }
+]);
